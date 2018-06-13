0ab6d0b93bd1d179fcbfbc743f207583a2058757
Trevor Brooks <trevorism@gmail.com>
2018-06-13 00:08:43 -0400

    Send event on completion of jenkins operations

--- src/main/groovy/com/trevorism/gcloud/webapi/controller/JenkinsController.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/controller/JenkinsController.groovy
@@ -1,9 +1,12 @@
 package com.trevorism.gcloud.webapi.controller
 
+import com.trevorism.event.WorkCompleteEventProducer
+import com.trevorism.event.model.WorkComplete
 import com.trevorism.gcloud.webapi.model.CreateJenkinsJob
 import com.trevorism.gcloud.webapi.model.JenkinsJob
 import com.trevorism.gcloud.webapi.service.DefaultJenkinsService
 import com.trevorism.gcloud.webapi.service.JenkinsService
+import com.trevorism.http.headers.HeadersHttpClient
 import com.trevorism.secure.Secure
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
@@ -15,6 +18,8 @@ import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.HttpHeaders
 import javax.ws.rs.core.MediaType
 
 /**
@@ -25,14 +30,18 @@ import javax.ws.rs.core.MediaType
 class JenkinsController {
 
     private JenkinsService jenkinsService = new DefaultJenkinsService()
+    private WorkCompleteEventProducer eventProducer = new WorkCompleteEventProducer()
 
     @ApiOperation(value = "Create a new jenkins job **Secure")
     @POST
     @Secure
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    boolean createJob(CreateJenkinsJob job){
-        jenkinsService.create(job)
+    boolean createJob(@Context HttpHeaders headers, CreateJenkinsJob job){
+        boolean result = jenkinsService.create(job)
+        String correlationId = headers?.getHeaderString(HeadersHttpClient.CORRELATION_ID_HEADER_KEY)
+        eventProducer.sendEvent(new WorkComplete("trevorism-gcloud", "cinvoke", correlationId))
+        return result
     }
 
     @ApiOperation(value = "Get a list of all jenkins jobs")
@@ -54,8 +63,11 @@ class JenkinsController {
     @DELETE
     @Secure
     @Consumes(MediaType.APPLICATION_JSON)
-    boolean deleteJob(JenkinsJob job){
-        jenkinsService.delete(job.name)
+    boolean deleteJob(@Context HttpHeaders headers, JenkinsJob job){
+        boolean result = jenkinsService.delete(job.name)
+        String correlationId = headers?.getHeaderString(HeadersHttpClient.CORRELATION_ID_HEADER_KEY)
+        eventProducer.sendEvent(new WorkComplete("trevorism-gcloud", "cinvoke", correlationId))
+        return result
     }
 
     @ApiOperation(value = "Invoke a job **Secure")
@@ -63,8 +75,11 @@ class JenkinsController {
     @Secure
     @Path("{job}/build")
     @Produces(MediaType.APPLICATION_JSON)
-    boolean invokeJob(@PathParam("job") String jobName){
-        jenkinsService.invoke(jobName)
+    boolean invokeJob(@Context HttpHeaders headers, @PathParam("job") String jobName){
+        boolean result = jenkinsService.invoke(jobName)
+        String correlationId = headers?.getHeaderString(HeadersHttpClient.CORRELATION_ID_HEADER_KEY)
+        eventProducer.sendEvent(new WorkComplete("trevorism-gcloud", "cinvoke", correlationId))
+        return result
     }
 
 }
